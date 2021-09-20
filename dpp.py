358e6f234f99d71fb07a08447536136dee62966b
Ayush <agrawal.ayush89@gmail.com>
2021-09-20 12:23:39 +1000

    first commit

--- dpp.py
+++ dpp.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 import unittest
 from bs4 import BeautifulSoup as BS
 from datetime import datetime
