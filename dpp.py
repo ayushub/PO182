715374aee09ed0c6c2c1c70d3a1fa95d8f04cc4b
Ayush <agrawal.ayush89@gmail.com>
2021-09-20 12:22:18 +1000

    first commit

--- dpp.py
+++ dpp.py
@@ -15,11 +15,11 @@ class dPTest(unittest.TestCase):
         coos = start.cookies.get_dict()
         coos["pc3o"] = 'd2r22'
         print(coos)
+
         bsoj = BS(start.text, 'html.parser')
         imgs = bsoj.find_all("img")
         # [src=filled0.gif]
         filled = filter(lambda x: x['src'].startswith('f'), imgs)
-
         c = len(filled)
         print(c)
         print(datetime.now(tz=None))
@@ -28,7 +28,7 @@ class dPTest(unittest.TestCase):
         resobj = 'title=submit&jobref=PO182&valuee=' + str(c)
         print(resobj)
         x = requests.post(url, data = resobj, cookies= coos, headers= headers)
-        
+
         print(datetime.now(tz=None))
         print(x.text)
 
